b20329cea636b87315c9709f18d800d8b9e6e90f
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 20:59:47 -0300

    Create Solving the Sum of Two Digits Programming Challenge.py

---
+++ Algorithmic Toolbox/Solving the Sum of Two Digits Programming Challenge.py
@@ -0,0 +1,12 @@
+# python3
+def entrence():
+    first_digit = int(input("A"))
+    second_digit = int(input("B"))
+    sum_of_two_digits(first_digit, second_digit)
+
+def sum_of_two_digits(first_digit, second_digit):
+     print(first_digit + second_digit)
+
+
+
+entrence()
