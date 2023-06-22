2e96602c5ece36faef99d94a354f1ddad7147f68
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:03:59 -0300

    Create Exercicios_Lista_Ordenada.py

---
+++ Curso USP/Exercicios_Lista_Ordenada.py
@@ -0,0 +1,13 @@
+#def entrada():
+ #   lista = [1, 3, 10, 110, 190, 330, 800]
+  #  ordenada(lista)
+
+def ordenada(lista):
+    if lista == sorted(lista):
+        #print(True)
+        return True
+    else:
+        #print(False)
+        return False
+
+#entrada()
