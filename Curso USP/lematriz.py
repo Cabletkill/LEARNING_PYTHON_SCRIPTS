426af3ba44fc64e5a96aec7e31c5600a9720a48b
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:04:05 -0300

    Create lematriz.py

---
+++ Curso USP/lematriz.py
@@ -0,0 +1,10 @@
+def cria_matriz(tot_lin, tot_col, valor):
+    matriz = []  #lista vazia
+    for i in range(tot_lin):
+        linha = []
+        for j in range(tot_col):
+            print(mat[i] "x" mat[j])
+            linha.append(valor)
+        matriz.append(linha)
+    return matriz
+mat= [[1], [2], [3]]
