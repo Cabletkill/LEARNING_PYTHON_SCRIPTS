f9796c22fdd02b14439cc03a9627c4a7269020d5
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:00:21 -0300

    Create edx_class01.py

---
+++ Curso EDX/edx_class01.py
@@ -0,0 +1,22 @@
+x= 1//2
+
+print(x)
+
+name=('Ricardo de Souza Silva')
+A=name.upper()
+
+print(len(name))
+print(A)
+
+
+
+print("hello Mike".find("Mike"))
+
+name = 'Lizz'
+
+print(name[0:2])
+
+Name="Michael Jackson"
+print(Name.find('el'))
+
+"123".replace("12", "ab")
