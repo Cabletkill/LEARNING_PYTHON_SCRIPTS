f9f870dbc8d725cae7b639ae929ea28b180d5a9d
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:01:51 -0300

    Create estudo_DIR_HELP.py

---
+++ Curso Udemy/estudo_DIR_HELP.py
@@ -0,0 +1,11 @@
+'''
+Utilitarios Python para auxiliar na programação
+dir->Apresenta todos os atributos e funções/métodos disponíveis para deteminado tipo de dado ou variavel
+
+help->Apresenta a documentaçõa/como utilizar os atributo/propriedades e funçães para determinado tipo de dado ou variável.
+'''
+
+
+
+
+print(dir(4))
