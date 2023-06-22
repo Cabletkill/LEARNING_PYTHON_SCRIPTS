3fdbc2aff2ed60dbb516e2670ac2acfa49986658
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:02:44 -0300

    Create explorando_ranges.py

---
+++ Curso Udemy/explorando_ranges.py
@@ -0,0 +1,19 @@
+''''
+range com passo que
+for num in range (valor_do_inicio, valor_da_parada, passo_que)
+tipo PARA numero in RANGE(1 a 10 de 2em2)
+     !!!!!for i in range(1,10,2):!!!!!
+
+     for i in range(0,50,2):
+         print(i)
+'''
+'''
+range com passo que inverso
+for num in range (valor_do_inicio, valor_da_parada, passo_que)
+tipo PARA numero in RANGE(10 a 0 de 1em1)
+     !!!!!for i in range(10, 0, -1)::!!!!!
+     
+'''
+for i in range(10, 0, -1):
+         print(i)
+
