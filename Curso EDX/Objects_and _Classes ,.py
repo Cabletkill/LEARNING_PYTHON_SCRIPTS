9f26a6b009a329421f2830c504cba108474f48ab
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:00:59 -0300

    Create Objects_and _Classes ,.py

---
+++ Curso EDX/Objects_and _Classes ,.py
@@ -0,0 +1,25 @@
+def step (x):
+    if x>0:
+        y=1
+    else:
+        y=0
+    return y
+def main():
+    x=20
+    print(step(x))
+main()
+
+'''
+class Circle(object):
+    def __init__(self, 10, 'red'):
+        self.radius=10;
+        self.color='red';
+        return 10
+
+    def __add__(self, r):
+       self.radius=self.radius+r
+       return (self.radius)
+'''
+
+
+
