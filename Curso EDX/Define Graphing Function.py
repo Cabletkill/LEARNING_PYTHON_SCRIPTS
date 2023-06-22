ac912d0e381f9dffed59d901fbe8af897fe37de0
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:00:03 -0300

    Create Define Graphing Function.py

---
+++ Curso EDX/Define Graphing Function.py
@@ -0,0 +1,45 @@
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bts
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021--06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+
+url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue?utm_medium=Exinfluencer&utm_source=Exinfluencer&utm_content=000026UJ&utm_term=10006555&utm_id=NA-SkillsNetwork-Channel-SkillsNetworkCoursesIBMDeveloperSkillsNetworkPY0220ENSkillsNetwork23455606-2021-01-01"
+
+stock_data  = requests.get(url).text
+html_data = bts ( stock_data, "html5lib")
+print(html_data.title)
+tesla_data = pd.DataFrame(columns=['Date', 'Open',  "High", "Low", "Close", "Volume"])
+
+print(tesla_data)
+for row in html_data.find("tbody").find_all('tr'):
+    col = row.find_all("td")
+    date = col[0].text
+    Open = col[1].text
+    high = col[2].text
+    low = col[3].text
+    close = col[4].text
+    adj_close = col[5].text
+    volume = col[6].text
+
+# Finally we append the data of each row to the table
+
+tesla_data = tesla_data.append({"Date":date, "Open":Open, "High":high, "Low":low, "Close":close, "Adj Close":adj_close, "Volume":volume}, ignore_index=True)
+print('Parte 01 = ',tesla_data.head())
