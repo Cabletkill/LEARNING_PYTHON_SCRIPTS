8dfb0453522c77fbce46d8a00e037af65635214f
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 05:07:26 -0300

    Create Lista_aula05.py

---
+++ Curso Udemy/Lista_aula05.py
@@ -0,0 +1,18 @@
+
+
+j =int(input("dig:"))
+
+
+listacores = [2**3,5,10*1,j**2]
+
+print(listacores[0])
+print(listacores[1])
+print(listacores[2])
+print(listacores[3])
+x = listacores[0] * listacores[1]
+print(x)
+print (sorted(listacores))#organiza sua lista
+
+print(list(enumerate(listacores)))#cria um numerador da lista
+
+print(listacores.index(5))# mostra em qual indice esta
