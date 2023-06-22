32dab31190eb8a2aa87647f872144c37fc18bbc2
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:01:54 -0300

    Create estruturas_condicionais.py

---
+++ Curso Udemy/estruturas_condicionais.py
@@ -0,0 +1,12 @@
+'''
+Estruturas condicionais
+if, elif, else:
+'''
+idade = int(input('Qual sua Idade :'))
+
+if idade < 18:
+    print("Menor de Idade")
+elif idade >18:
+    print('Maior de Idade')
+else:
+    print('Idade igual')
