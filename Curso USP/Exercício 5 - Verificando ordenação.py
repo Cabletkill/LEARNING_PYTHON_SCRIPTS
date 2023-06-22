b54c6186aa7342d0a219396fdb81c682eae7dc0c
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:03:15 -0300

    Create Exercício 5 - Verificando ordenação.py

---
+++ Curso USP/Exercício 5 - Verificando ordenação.py
@@ -0,0 +1,9 @@
+a=int(input('digite o numero: '))
+b=int(input('digite o numero: '))
+c=int(input('digite o numero: '))
+
+if a < b and b < c:
+    print('crescente')
+
+else:
+    print('não está em ordem crescente')
