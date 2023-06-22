c81e5cbec4228ee2be1de2b44c50af7f1ad602d6
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:02:12 -0300

    Create contagem_lista_nomes.py

---
+++ Curso USP/contagem_lista_nomes.py
@@ -0,0 +1,20 @@
+lista = ['o', 'gato', 'caçava', 'rato','o']
+
+print (lista[0])
+
+
+x = 1
+for n in range(1, len(lista)):
+    if lista[n]==lista[x]:
+       print (lista)
+    else:
+        x = x + 1
+
+
+
+
+
+
+
+
+
