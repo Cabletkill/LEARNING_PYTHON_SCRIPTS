911b86d5c7cb64ad2c8814921c7c10963721d8c1
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 05:07:44 -0300

    Create Problema_Contador_Horas_Minutos_Segundos.py

---
+++ Curso USP/Problema_Contador_Horas_Minutos_Segundos.py
@@ -0,0 +1,11 @@
+
+segundos_str = int(input(' Entre com os segundos para converter: '))
+
+horas = segundos_str // 3600
+segrestante = segundos_str % 3600
+minutos= segrestante // 60
+final= segrestante % 60
+dias =  horas  // 24
+restohoras = (segundos_str // 3600)- (dias * 24)
+
+print(dias, 'dias', restohoras, 'Horas', minutos, 'Minutos e', final, 'Segundos.' )
