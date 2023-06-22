3aa12fb03154c3483806cef5636ffc193faef715
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:01:33 -0300

    Create 50_Entendendo_kwargs.py

---
+++ Curso Udemy/50_Entendendo_kwargs.py
@@ -0,0 +1,13 @@
+def Divisão_Inversa ():
+    x = int(input("Dividir o número:"))
+    numeros(a, b ,c)
+
+def numeros(a, b, c):
+    a = int(input('1ºNumero:'))
+    b = int(input('2ºNumero:'))
+    a = int(input('3ºNumero:'))
+    return a, b, c
+    
+
+
+Divisão_Inversa()
