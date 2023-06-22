98cfd08c982b0faf020337d27f2c24583da04c7e
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:03:31 -0300

    Create Exercício_retangulos.py

---
+++ Curso USP/Exercício_retangulos.py
@@ -0,0 +1,10 @@
+x=int(input("Digite a Largura:"))
+y=int(input("Digite a Altura:"))
+repetelinha = 0
+repetecoluna = 0
+while repetecoluna<y:
+      repetecoluna=repetecoluna+1
+      leg='#'
+      while repetelinha<x:
+            repetelinha=repetelinha+1
+      print(leg*repetelinha)
