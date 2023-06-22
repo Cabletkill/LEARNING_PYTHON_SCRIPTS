ab778ddfdab68180a87630ee9efa15c426fac924
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:03:52 -0300

    Create introducao_def.py

---
+++ Curso USP/introducao_def.py
@@ -0,0 +1,9 @@
+def nomeidade():
+    nome=input('Coloque o Nome:')
+    idade=int(input('Coleque a Idade: '))
+
+    print(nome,'tem a idade igual a', idade, '\nPróximo!!!\n')
+
+nomeidade()
+nomeidade()
+nomeidade()
