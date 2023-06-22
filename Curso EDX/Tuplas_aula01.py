00575e4d6da31a7a38049640ca060977fdd968e7
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-21 21:01:20 -0300

    Create Tuplas_aula01.py

---
+++ Curso EDX/Tuplas_aula01.py
@@ -0,0 +1,15 @@
+'''
+1º Tuplas são imutaveis ou seja cada operação em uma tupla gera uma nova tupla
+2º Listas são mutaveis podendo mudar costantemente
+'''
+
+#CUIDADO 1: AS TUPLS SÃO REPRESENTADAS POR (), MAS;
+
+tupla01 = (1,2,3,4,5,6) #exemplo de tupla 01
+print(type(tupla01))
+print(tupla01)
+
+tupla02 = 1, 2, 3, 4, 5, 6, 7 #exemplo de tupla 02
+print(tupla02)
+print(type(tupla02))
+
