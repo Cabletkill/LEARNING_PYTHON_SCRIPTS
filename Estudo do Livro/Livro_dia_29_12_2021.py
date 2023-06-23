e9a48d5c979c20b81ca7c56f942ace25f1e8a81f
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:50:09 -0300

    Create Livro_dia_29_12_2021.py

---
+++ Estudo do Livro/Livro_dia_29_12_2021.py
@@ -0,0 +1,13 @@
+red = 1
+red += 2
+print(red)
+red -= 0
+print(red)
+red /= 2
+print(red)
+print()
+coma = 'Ricardo'
+print('Helo')
+coma += ' Silva'
+print('Hello ' + coma)
+print(coma.index('Ricardo'))
