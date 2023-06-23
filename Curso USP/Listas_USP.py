4b209a2dc72975637e2fbce4ce9d09d9ff68819f
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:50:39 -0300

    Create Listas_USP.py

---
+++ Curso USP/Listas_USP.py
@@ -0,0 +1,15 @@
+'''
+como trabalhar com listas
+'''
+lista= [10,'voce','coma','jose','joao']
+#x=int(input("Digita numero:"))
+list=lista+[10,20,30]
+v=lista[0]+1
+print(v)
+list01=list+[11,22,33]
+list01.append('Como vai voce')
+print(len(list01))
+s=[]
+#s.append(x)
+s.append(list01)
+print(s)
