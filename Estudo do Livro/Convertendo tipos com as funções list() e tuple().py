242f04e6061bab9b78234de423e2ca18bbc2036c
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:49:45 -0300

    Create Convertendo tipos com as funções list() e tuple().py

---
+++ Estudo do Livro/Convertendo tipos com as funções list() e tuple().py
@@ -0,0 +1,14 @@
+lista_convert = tuple(['Cat','Dog',5,6])
+print(lista_convert)
+print(type(lista_convert))
+lista_convert02 = list(['Cat','Dog',5,6])
+print(lista_convert02)
+print(type(lista_convert02))
+hello = list('Hello')
+print(hello)
+
+print()
+spam = [0,1,2,3,4,5,6,7,8,9]
+cheese = spam
+cheese[1]="hello" # efetuou a troca de caractere dentro da lista
+print(spam)
