2f459b9ca6e25402705dbdfafdda4777a96fff91
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:51:10 -0300

    Create Lista_aula07.py

---
+++ Curso Udemy/Lista_aula07.py
@@ -0,0 +1,11 @@
+lista = [1,2,3,4,10,8,7,8,99,10,121,12,213,14,3315,30 ]
+lista02 = [16,15617,18,15559,20,21,22,23,24,25,26,27,28,29]
+print(lista)
+
+nova = lista.copy()
+print(nova)
+print()
+
+nova.append(10)
+print(lista)
+print(nova)
