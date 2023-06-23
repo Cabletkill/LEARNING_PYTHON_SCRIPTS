38fc386084f5911ef426945ab1002a439fb8a32c
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:48:58 -0300

    Create Programacao_Orientada_a_Objetos _Parte_4.1.py

---
+++ Curso USP/Programacao_Orientada_a_Objetos _Parte_4.1.py
@@ -0,0 +1,24 @@
+class Triangulo:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def perimetro(self):
+        return self.a+self.b+self.c
+
+
+
+t = Triangulo(1,2,3)
+t.perimetro()
+
+
+
+
+
+
+
+
+
+
+
