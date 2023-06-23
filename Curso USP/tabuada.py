6cf627cd38ade002d475d22213ded227b4d12f5b
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:49:29 -0300

    Create tabuada.py

---
+++ Curso USP/tabuada.py
@@ -0,0 +1,57 @@
+'''
+x = 1
+cont = 0
+while x < 3:
+    y = 0
+    while y <= 4:
+        # Iteração
+        y = y + 1
+        print('2')
+    x = x + 1
+'''
+''''
+fora = 5
+while fora > 0:
+  dentro = 0
+  while dentro < fora:
+    print("oi")
+    dentro = dentro + 1
+  fora = fora - 1
+'''
+'''
+x = 2
+cont = 0
+while x >= 0:
+    y = 0
+    while y >= 4:
+          print("oi")
+          y = y + 1
+    x = x - 1
+x = 2
+cont = 0
+'''
+'''
+while x >= 0:
+    y = 0
+    while y <= 4:
+        print("oi")
+        y = y - 1
+    x = x - 1
+'''
+'''
+i = 0
+while i < 3:
+  j = 0
+  while j < 3:
+    print(i + j, end=" ")
+    j = j + 1
+  i = i + 1
+print()
+'''
+i = 0
+while i < 3:
+  j = 0
+  while j < 3:
+    print(3 * i + j + 1, end=" ")
+    j = j + 1
+  i = i + 1
