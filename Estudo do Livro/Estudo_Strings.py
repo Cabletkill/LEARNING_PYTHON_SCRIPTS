4edd83a1472d8d1cb2d0fb68e802690b14256c37
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:49:47 -0300

    Create Estudo_Strings.py

---
+++ Estudo do Livro/Estudo_Strings.py
@@ -0,0 +1,22 @@
+word = 'Banana'
+count = 0
+for i in range(len(word)):
+    if word[i] =="a":
+        count = count+1
+print(count)
+
+index = word.find('a')
+
+print(index)
+
+print('a' in 'Macaco')
+print('seed' in "macarena" )
+
+print()
+print()
+
+def in_both(word1, word2):
+    for letter in word1:
+        if letter in word2:
+            print(letter)
+x = in_both("macacacco",'macacc')
