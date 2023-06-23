d354a44d799fe50ee7342787333706ae2e883f9b
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:49:31 -0300

    Create testando projetos.py

---
+++ Curso USP/testando projetos.py
@@ -0,0 +1,18 @@
+def main():
+
+     #começando o teste de string A e B
+
+ A=input('Digite o primeiro numero:')
+ B=input("Digite o Segundo Numero:")
+
+     ##     A e B guardam numero inteiros
+ A=int(A)#converte string/texto para numero inteiro
+ B=int(B)#converte string/texto para numero inteiro
+
+      #segue o calculo padrao da serie
+ soma= A+B
+
+ # imprimir a soma
+ print('A soma de', A, "+",B,'Eh igual a', soma)
+
+main()
