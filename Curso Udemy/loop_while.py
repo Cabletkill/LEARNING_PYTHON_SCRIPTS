d6e98261cd615d69bf684a0a260d43aa9feee511
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:51:01 -0300

    Create loop_while.py

---
+++ Curso Udemy/loop_while.py
@@ -0,0 +1,13 @@
+''''
+x=int(input('Digito o numero a ser multiplicado: '))
+y=int(input("Digite quantos multiplos: "))
+            \\\\\\\\\\\\\\teste multiplos\\\\\\\\\\\\\\\\
+for i in range(1,y+1):
+    soma=x*i
+    print('Numero:',i ,'=', soma)
+'''
+cont=0
+while not cont==3:
+      cont=cont+1
+      for i in range (1,100+1,1):
+             print (f'',i,)
