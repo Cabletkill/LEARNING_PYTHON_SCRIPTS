3fe32e8042056811fafea7d31d94625ab0a66f83
Ricardo de Souza <84940302+Cabletkill@users.noreply.github.com>
2023-06-22 21:50:11 -0300

    Create Livro_dia_29_12_2021_Metodo_Index.py

---
+++ Estudo do Livro/Livro_dia_29_12_2021_Metodo_Index.py
@@ -0,0 +1,11 @@
+spam = ['Hello', 'Hi', 'Howdy', "Heyas",'Zophie', 'Pooka', 'Fat-tail', 'Pooka']
+print()
+spam.sort()
+print(spam)
+spam.sort(reverse=True)
+print(spam)
+lista = ['Hello', 'Hi', 'Howdy', "Heyas",'Zophie', 'Pooka', 'Fat-tail', 'Pooka',1 ,2 ,3 ,33,56]
+lista.sort()# não é possivel arranjar uma lista em ordem crescente ou decrescente caso haja str e numero
+print(lista)
+
+
